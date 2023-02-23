0de947ca30946c87cd0db6c256a86849df495b37
t-flim <fulim1988@gmail.com>
2023-02-23 11:03:17 +0800

    Add App structure code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
     <div className="app">
+      <header id="header"></header>
+      <main id="main">
+        <textarea id="input" />
+        <div id="preview"></div>
+      </main>
+      <footer id="footer"></footer>
     </div>
   )
 }
