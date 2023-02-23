da80da4507f0fc0cd696c61ea7cc9c2ee5e57ef0
t-flim <fulim1988@gmail.com>
2023-02-23 11:08:39 +0800

    Add handleInput function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 
 function App() {
 
+  const handleInput = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="app">
       <header id="header"></header>
       <main id="main">
-        <textarea id="input" />
+        <textarea id="editor" onChange={handleInput} />
         <div id="preview"></div>
       </main>
       <footer id="footer"></footer>
